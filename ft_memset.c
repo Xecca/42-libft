958257db975a2e760bb3cec58df0c6bf63b98b9d
Andrew Po <ensimgen@gmail.com>
2019-05-21 21:39:28 -0700

    Add files via upload
    checked norminette

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,26 @@
 /*   By: aponomar <aponomar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 16:35:59 by aponomar          #+#    #+#             */
-/*   Updated: 2019/05/21 16:36:00 by aponomar         ###   ########.fr       */
+/*   Updated: 2019/05/21 21:38:54 by aponomar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void				*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*str;
+	int				i;
+
+	if (b != NULL)
+	{
+		str = (unsigned char*)b;
+		i = 0;
+		while (len > i)
+		{
+			str[i] = c;
+			i++;
+		}
+		return (str);
+	}
+}
