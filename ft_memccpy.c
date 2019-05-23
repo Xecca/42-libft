3b18402ed3b351047abb73e4bda0bb23e46f8a01
Andrew Po <ensimgen@gmail.com>
2019-05-22 18:42:27 -0700

    Update ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -10,3 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memccpy(void *restrict dst,\
+		const void *restrict src, int c, size_t n)
+{
+	while (n)
+	{
+		*(unsigned char*)dst = *(unsigned char*)src;
+		if (*(unsigned char*)src == (unsigned char)c)
+			return (++dst);
+		dst++;
+		src++;
+		n--;
+	}
+	return (NULL);
+}
