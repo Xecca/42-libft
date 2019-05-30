57b34171923c88209f0fc6976331983e649d28e5
Andrew Po <ensimgen@gmail.com>
2019-05-29 22:30:18 -0700

    Update and rename nm ft_putstr.c to ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -14,9 +14,15 @@
 
 void	ft_putstr(char *str)
 {
+	int i;
+
+	i = 0;
 	if (str != NULL)
 	{
-		while (*str)
-			write(1, str++, 1);
+		while (str[i] != '\0')
+		{
+			write(1, &str[i], 1);
+			i++;
+		}
 	}
 }
