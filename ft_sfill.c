c671987591eb3efc4c2315924a5560f370fc3288
Andrew Po <ensimgen@gmail.com>
2019-06-05 19:34:38 -0700

    Update and rename nm ft_sfill.c to ft_sfill.c

--- ft_sfill.c
+++ ft_sfill.c
@@ -6,36 +6,36 @@
 /*   By: aponomar <aponomar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 13:06:27 by aponomar          #+#    #+#             */
-/*   Updated: 2019/05/23 13:06:32 by aponomar         ###   ########.fr       */
+/*   Updated: 2019/06/05 19:32:13 by aponomar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_sfill(char const *s, char **main_str, int wdcount, char c)
+char	**ft_sfill(char const *s, char **str, int wdcount, char c)
 {
 	int	j;
-	int	m;
-	int	c_counter;
+	int	r;
+	int	ch_count;
 
-	m = 0;
+	r = 0;
 	while (wdcount--)
 	{
-		c_counter = ft_chcount(s, c);
+		ch_count = ft_chcount(s, c);
 		while (*s == c)
 			s++;
-		while (c_counter > 0)
+		while (ch_count--)
 		{
 			j = 0;
-			main_str[m] = (char*)malloc(c_counter + 1);
-			while (c_counter--)
+			str[r] = (char*)malloc(ch_count + 1);
+			while (ch_count--)
 			{
-				main_str[m][j++] = *s;
+				str[r][j++] = *s;
 				s++;
 			}
-			main_str[m++][j] = '\0';
+			str[r++][j] = '\0';
 		}
 	}
-	main_str[m] = NULL;
-	return (main_str);
+	str[r] = NULL;
+	return (str);
 }
