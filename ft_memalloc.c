27b7dc08ca24a4bd6d702959ebcf1f0410d86f2e
Andrew Po <ensimgen@gmail.com>
2019-06-06 14:39:52 -0700

    Update and rename nm ft_memalloc.c to ft_memalloc.c

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -16,7 +16,7 @@ void	*ft_memalloc(size_t size)
 {
 	char	*str;
 
-	str = (char*)malloc(sizeof(int) * size);
+	str = (char*)malloc(sizeof(char) * size);
 	if (str == NULL)
 		return (NULL);
 	ft_bzero(str, size);
