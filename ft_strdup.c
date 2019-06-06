d577ad46adda942c59f10f7f9ee23f1c49be61f3
Andrew Po <ensimgen@gmail.com>
2019-06-05 23:17:59 -0700

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -17,7 +17,7 @@ char	*ft_strdup(const char *s1)
 	char	*dup;
 	size_t	i;
 
-	dup = malloc(sizeof(*s1) * (ft_strlen(s1) + 1));
+	dup = malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (dup == NULL)
 		return (NULL);
 	i = 0;
