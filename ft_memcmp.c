64fa4d5a877a998328f030d93b62d513b34f6e5f
Andrew Po <ensimgen@gmail.com>
2019-06-07 15:15:52 -0700

    Update and rename nm ft_memcmp.c to ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,12 +14,13 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	while (n--)
+	while (n > 0)
 	{
 		if (*((unsigned char*)s1) != *((unsigned char*)s2))
 			return (*((unsigned char*)s1) - *((unsigned char*)s2));
 		s1++;
 		s2++;
+		n--;
 	}
 	return (0);
 }
