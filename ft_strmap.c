77cd7936d2bca6bc03bda5f478e5d1472d1c2596
Andrew Po <ensimgen@gmail.com>
2019-06-08 15:13:37 -0700

    Update and rename nm ft_strmap.c to ft_strmap.c

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,33 +6,30 @@
 /*   By: aponomar <aponomar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 18:47:06 by aponomar          #+#    #+#             */
-/*   Updated: 2019/05/22 19:14:26 by aponomar         ###   ########.fr       */
+/*   Updated: 2019/06/08 15:11:33 by aponomar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmap(char const *s, char (*f)(char))
+char		*ft_strmap(char const *s, char (*f)(char))
 {
 	char	*str;
-	size_t	len;
 	size_t	i;
+	size_t	len;
 
-	if (f == NULL || s == NULL)
+	if (s == NULL || f == NULL)
 		return (NULL);
 	i = 0;
 	len = ft_strlen(s);
 	str = (char*)malloc(len + 1);
 	if (str == NULL)
 		return (NULL);
-	if (s != NULL && f != NULL)
+	while (i < len)
 	{
-		while (i < len)
-		{
-			str[i] = (*f)(s[i]);
-			i++;
-		}
-		str[i] = '\0';
+		str[i] = (*f)(s[i]);
+		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
