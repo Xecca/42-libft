f3709684239e58c8c59e7929818e350d269b1814
Andrew Po <ensimgen@gmail.com>
2019-06-08 18:56:55 -0700

    Update nm ft_strsub.c

--- nm ft_strsub.c
+++ nm ft_strsub.c
@@ -15,12 +15,10 @@
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
-	size_t	s_len;
 	size_t	i;
 
 	if (s == NULL)
 		return (NULL);
-	s_len = (len - start) + 1;
 	i = 0;
 	str = (char*)malloc((len) + 1);
 	if (str == NULL)
